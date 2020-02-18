48127ae37b5f982d8e1c7881ff9e62d1609ef3b2
Will Traweek <will.traweek@gmail.com>
2020-02-18 09:33:21 -0600

    add zirtue_forwarding_page

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def home_page():
     return render_template("homepage.html")
 
+@app.route('/zirtue_forwarding_test')
+def zirtue_forwarding_page():
+    return render_template("zirtue_forwarding_test.html")
+
 #TODO: include a 404 page
 #TODO: add resume to homepage
 #TODO: tab at the top referencing medium blog
